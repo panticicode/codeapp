660aabe16e500131aad3cae1a3c72cbf1f2221d6
someone <someone@someplace.com>
2019-08-19 23:08:40 +0200

    DISQUS

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -40,85 +40,29 @@
 		{!! Form::close() !!}
 	</div>
 	<hr>
-	<!-- Posted Comments -->
-	@endif
-	@if(count($comments) > 0)
-		<!-- Comment -->
-		@if(Session :: has('reply_message'))
-			{{session('reply_message')}}
-		@endif
-		@foreach($comments as $comment)
-		<div class="media">
-			<a class="pull-left" href="#">
-				<img height="64" class="media-object" src="{{$comment->photo}}" alt="">
-				<!--IF YOU WANT A USE GRAVATAR YOU CAN-->
-				<!--<img height="64" class="media-object" src="{{Auth::user()->gravatar}}" alt="">-->
-			</a>
-			<div class="comment-replay-container">
-			<button class="toggle-replay btn btn-primary pull-right">Replay</button>
-				<div class="comment-replay col-sm-12">
-				{!! Form :: open(['method'=>'POST', 'action'=>'CommentRepliesController@createReplay']) !!}
-				<input type="hidden" name="comment_id" value="{{$comment->id}}">
-				<div class="form-group">
-					{!! Form :: label('body', 'Body:') !!}
-					{!! Form :: textarea('body', null, ['class'=>'form-control', 'rows'=>1]) !!}
-				</div>
-				<div class="form-group">
-					{!! Form :: submit('Submit', ['class'=>'btn btn-primary'])!!}
-				</div>
-				{!! Form :: close() !!}
-				</div>
-			</div>
-			<div class="media-body">
-				<h4 class="media-heading">{{$comment->author}}
-					<small>{{$comment->created_at->diffForHumans()}}</small>
-				</h4>
-				<p>{{$comment->body}}</p>
-				<!-- Nested Comment -->
-			@if(count($comment->replies) > 0)	
-			@foreach($comment->replies as $reply)
-				@if($reply->is_active == true)
-				<div id="nested-comment" class="media">
-					<a class="pull-left" href="#">
-						<img height="64" class="media-object" src="{{$reply->photo}}" alt="">
-					</a>
-					<div class="media-body">
-						<h4 class="media-heading">{{$reply->author}}
-							<small>{{$reply->created_at->diffForHumans()}}</small>
-						</h4>
-						{{$reply->body}}
-					</div>
-					<div class="comment-replay-container">
-					<button class="toggle-replay btn btn-primary pull-right">Replay</button>
-						<div class="comment-replay col-sm-9">
-						{!! Form :: open(['method'=>'POST', 'action'=>'CommentRepliesController@createReplay']) !!}
-						<input type="hidden" name="comment_id" value="{{$comment->id}}">
-						<div class="form-group">
-							{!! Form :: label('body', 'Body:') !!}
-							{!! Form :: textarea('body', null, ['class'=>'form-control', 'rows'=>1]) !!}
-						</div>
-						<div class="form-group">
-							{!! Form :: submit('Submit', ['class'=>'btn btn-primary'])!!}
-						</div>
-						{!! Form :: close() !!}
-						</div>
-					</div>
-				</div>
-				<!-- End Nested Comment -->
-				@else
-					<!--<h1 class="text-center">No Replies</h1>-->
-				@endif
-			@endforeach
-		@endif
-		</div>
-	</div>
-	@endforeach
-	@endif
-@stop
-@section('scripts')
-<script>
-	$(".comment-replay-container .toggle-replay").click(function(){
-		$(this).next().slideToggle("slow");
-	});
-</script>
+	
+<!--DISQUS-->
+<!--
+<div id="disqus_thread"></div>
+	<script>
+
+	/**
+	*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+	*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+	/*
+	var disqus_config = function () {
+	this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+	this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+	};
+	*/
+	(function() { // DON'T EDIT BELOW THIS LINE
+	var d = document, s = d.createElement('script');
+	s.src = 'https://codeapp-test.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+	})();
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	<script id="dsq-count-scr" src="//codeapp-test.disqus.com/count.js" async></script>
+--><!--DISQUS-->
 @stop
